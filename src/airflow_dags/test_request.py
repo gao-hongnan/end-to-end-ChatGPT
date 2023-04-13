8b43149f0093687444b09c48283640450d7137e6
gaohn <reighns.sjr.sjh.gr.twt.hrj@gmail.com>
2023-04-13 19:41:34 +0800

    airflow: successful fetch and insert data into database by grouping all functions in the same script.

--- src/airflow_dags/test_request.py
+++ src/airflow_dags/test_request.py
@@ -1,9 +1,14 @@
+from __future__ import annotations
 import json
 import os
 import subprocess
 import sys
 import traceback
 from typing import Any, Dict, List
+from typing import Any, Optional, Type, TypeVar, Tuple
+
+import psycopg2
+from psycopg2.extensions import connection
 
 import httpx
 import pendulum
@@ -16,9 +21,75 @@ from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 # temp workaround for src module import
 # sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from src.database.client import Connection, DatabaseConnection
-from src.database.fetch_store_train_evaluate import save_data_to_db
+# sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# from src.database.client import Connection, DatabaseConnection
+# from src.database.fetch_store_train_evaluate import save_data_to_db
+import psycopg2
+
+Connection = TypeVar("Connection", bound=connection)
+
+# pylint: disable=redefined-outer-name, too-many-arguments
+class DatabaseConnection:
+    """Singleton class for database connection."""
+
+    _instance: Optional[DatabaseConnection] = None
+
+    def __new__(
+        cls: Type[DatabaseConnection], *args: Any, **kwargs: Any
+    ) -> DatabaseConnection:
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.init_connection(*args, **kwargs)
+        return cls._instance
+
+    def init_connection(
+        self, user: str, password: str, host: str, port: str, database: str
+    ) -> None:
+        """
+        Initialize the database connection.
+
+        :param user: The database user.
+        :param password: The password for the database user.
+        :param host: The database host.
+        :param port: The port to connect to the database.
+        :param database: The name of the database.
+        """
+        # pylint: disable=attribute-defined-outside-init
+        self.conn: Connection = psycopg2.connect(
+            user=user, password=password, host=host, port=port, database=database
+        )
+
+    def get_connection(self) -> Connection:
+        """
+        Get the database connection.
+
+        :return: The psycopg2 connection object.
+        """
+        return self.conn
+
+    def close(self) -> None:
+        """Close the database connection."""
+        if self.conn:
+            self.conn.close()
+
+
+def execute_query(
+    connection: Connection, query: str, params: Tuple[Any, ...] = ()
+) -> None:
+    cur = connection.cursor()
+
+    cur.execute(query, params)
+
+    connection.commit()
+    cur.close()
+
+
+def save_data_to_db(
+    insert_query: str, data: List[Dict[str, Any]], connection: Connection
+) -> None:
+    for item in data:
+        params = tuple(item.values())
+        execute_query(connection, insert_query, params)
 
 
 @dag(
